0f0566a2d4148f1fe311269f2710feb9acae8012
¨Даниил¨ <daniilmirlanuulu@gmail.com>
2024-04-15 19:24:13 +0600

    allauth

--- car/web/admin.py
+++ car/web/admin.py
@@ -2,7 +2,6 @@ from django.contrib import admin
 
 from .models import *
 
-admin.site.register(Model)
+admin.site.register(Car)
 admin.site.register(Marka)
-admin.site.register(Product)
 admin.site.register(CarBet)
